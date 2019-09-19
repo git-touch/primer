8b87a5edc5205dd91681fde4f9d01c39b0edc398
Rongjian Zhang <pd4d10@gmail.com>
2019-09-19 14:48:47 +0800

    feat(gallery): add code

--- primer_gallery/lib/main.dart
+++ primer_gallery/lib/main.dart
@@ -24,19 +24,85 @@ class WidgetItem {
   String name;
   String description;
   Widget Function() screenFactory;
-  WidgetItem(this.name, this.description, this.screenFactory);
+  String code;
+  WidgetItem(this.name, this.description, this.screenFactory, this.code);
 }
 
 List<WidgetItem> widgetItems = [
-  WidgetItem('Alert', 'Inform users of successful or pending actions',
-      () => AlertScreen()),
   WidgetItem(
-      'Blankslate',
-      'Used when there is a lack of content within a page or section',
-      () => BlankslateScreen()),
-  WidgetItem('Branch Name', 'Git branch name', () => BranchNameScreen()),
+    'Alert',
+    'Inform users of successful or pending actions',
+    () => AlertScreen(),
+    '''import 'package:flutter/material.dart';
+import 'package:primer/primer.dart';
+
+class AlertScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Alert('Flash message goes here.');
+  }
+}
+''',
+  ),
   WidgetItem(
-      'Label', 'Add contextual metadata to a design', () => LabelScreen()),
+    'Blankslate',
+    'Used when there is a lack of content within a page or section',
+    () => BlankslateScreen(),
+    '''import 'package:flutter/material.dart';
+import 'package:primer/primer.dart';
+
+class BlankslateScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        BlankslateTitle('This is a blank slate'),
+        Text('Use it to provide information when no dynamic content exists.'),
+      ],
+    );
+  }
+}
+''',
+  ),
+  WidgetItem(
+    'Branch Name',
+    'Git branch name',
+    () => BranchNameScreen(),
+    '''import 'package:flutter/material.dart';
+import 'package:primer/primer.dart';
+
+class BranchNameScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BranchName('a_new_feature_branch');
+  }
+}
+''',
+  ),
+  WidgetItem(
+    'Label',
+    'Add contextual metadata to a design',
+    () => LabelScreen(),
+    '''import 'package:flutter/material.dart';
+import 'package:primer/primer.dart';
+
+class LabelScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 4,
+      children: <Widget>[
+        Label('default label', theme: PrimerTheme.primary),
+        Label('default label', theme: PrimerTheme.primary, outline: true),
+        StateLabel(StateLabelStatus.issueOpened),
+        StateLabel(StateLabelStatus.issueClosed),
+        StateLabel(StateLabelStatus.pullMerged),
+      ],
+    );
+  }
+}
+''',
+  ),
 ];
 
 class MyHomePage extends StatelessWidget {
@@ -60,7 +126,21 @@ class MyHomePage extends StatelessWidget {
               Navigator.of(context).push(MaterialPageRoute(
                 builder: (context) => Scaffold(
                   appBar: AppBar(title: Text(item.name)),
-                  body: item.screenFactory(),
+                  body: Column(
+                    children: [
+                      Center(
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 20),
+                          child: item.screenFactory(),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        child: Text(item.code,
+                            style: TextStyle(fontFamily: 'Menlo')),
+                      ),
+                    ],
+                  ),
                 ),
               ));
             },
