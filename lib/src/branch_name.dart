27ea561c5b7b5f07ec0c80efc26b11e0d18fb708
Rongjian Zhang <pd4d10@gmail.com>
2019-11-17 11:42:19 +0800

    fix: branch name style

--- lib/src/branch_name.dart
+++ lib/src/branch_name.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/widgets.dart';
 import 'colors.dart';
 
-const branchBgColor = Color(0xffeaf5ff);
-
 class BranchName extends StatelessWidget {
   final String name;
 
   BranchName(this.name);
 
+  static const branchBgColor = Color(0xffeaf5ff);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(vertical: 2, horizontal: 6),
+      height: 18,
       decoration: BoxDecoration(
         color: branchBgColor,
         borderRadius: BorderRadius.all(Radius.circular(3)),
       ),
-      child: Text(name,
-          style: TextStyle(color: PrimerColors.blue500, fontSize: 12)),
+      child: Text(
+        name,
+        style: TextStyle(
+            color: PrimerColors.blue500,
+            fontSize: 12,
+            fontFamily: 'monospace'), // TODO: monospace fonts
+      ),
     );
   }
 }
